c547089071b3944d673366626e20a4158546b305
summerlinrb <78665001+summerlinrb@users.noreply.github.com>
2021-07-07 16:27:35 -0500

    Add files via upload

---
+++ FlattopBarberShop/scripts.js
@@ -0,0 +1,30 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using anime.js
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                anime({
+                    targets: 'html, body',
+                    scrollTop: target.offset().top - 56,
+                    duration: 1000,
+                    easing: 'easeInOutExpo'
+                });
+                return false;
+            }
+        }
+    });
+
+})(jQuery); // End of use strict
+
+
+
